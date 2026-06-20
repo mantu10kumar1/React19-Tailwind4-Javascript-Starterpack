66c559de578e1f0c15022cdb5b5127d82dfa52ff
mantu10kumar1 <mantukumar4376@gmail.com>
2026-06-20 19:26:58 +0530

    I have create starter project react , typescript and tailwind

---
+++ react-typescript-tailwind/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-5xl font-bold text-blue-600">
+        React 19 + Tailwind CSS
+      </h1>
+    </div>
+  )
+}
+
+export default App
