3470529534c235f97719f27e65620a9f582e6c38
mantu10kumar1 <mantukumar4376@gmail.com>
2026-06-20 19:10:05 +0530

    This starterpack project of React19 and tailwind4

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-5xl font-bold text-blue-600">
+        React 19 + Tailwind CSS
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
